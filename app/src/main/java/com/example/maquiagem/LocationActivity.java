b643c9b4e801f7bbf2325672903e6bc8720ced2d
GuilhermePalma <guilherme.palma8@etec.sp.gov.br>
2021-06-11 15:58:29 -0300

    Alteracoes no Layout e Strings da Localizacao

--- app/src/main/java/com/example/maquiagem/LocationActivity.java
+++ app/src/main/java/com/example/maquiagem/LocationActivity.java
@@ -2,22 +2,28 @@ package com.example.maquiagem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
 import java.util.List;
@@ -26,7 +32,8 @@ import java.util.Locale;
 public class LocationActivity extends AppCompatActivity {
 
 
-    private TextView showAdress;
+    private TextView showAddress;
+    private Toolbar toolbar;
     public double latitude;
     public double longitude;
 
@@ -35,11 +42,54 @@ public class LocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
+        // Criação da ToolBar e Criação da seta de voltar
+        toolbar = findViewById(R.id.toolBar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_return_home);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        // Controla os serviços de Localziação
+        LocationManager service = (LocationManager)
+                getSystemService(LOCATION_SERVICE);
+
+        //Valida a conexão com Internet
+        ConnectivityManager connectionManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Verifica se o GPS está ativo e se possui conexão com a Internet
+        boolean gpsIsEnabled = false;
+
+        gpsIsEnabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // Caso o Conection Manager não tenha sido Inciado (Defalult = Null)
+        if (connectionManager == null){
+            Snackbar notInternet =  Snackbar.make(
+                    findViewById(R.id.layout_location),
+                    R.string.error_connection,
+                    Snackbar.LENGTH_LONG);
+            notInternet.show();
+        } else if (!gpsIsEnabled){
+            Snackbar noGps =  Snackbar.make(
+                    findViewById(R.id.layout_location),
+                    R.string.error_noGps,
+                    Snackbar.LENGTH_LONG);
+            noGps.show();
+
+        } else{
+            // Pega a localização do Usuario
+            getLastLocationUser();
+        }
+
     }
 
-    public void pickLocation(View v) {
 
-        showAdress = findViewById(R.id.txt_location);
+    // Metodo que recupera a Ultima Localização Conhecida
+    public void getLastLocationUser() {
+
+        showAddress = findViewById(R.id.txt_location);
 
         // Serviço do Google para Manipular Localizações
         FusedLocationProviderClient fusedLocationClient =LocationServices.
@@ -54,9 +104,8 @@ public class LocationActivity extends AppCompatActivity {
 
             return;
         } else {
-            Log.d("PERMISSÕES", "Permitido o uso do Local ");
+            Log.d("PERMISSÕES", "\nPermitido o uso do Local ");
         }
-
 
         // Recupera a Ultima Localização
         fusedLocationClient.getLastLocation().
@@ -80,28 +129,27 @@ public class LocationActivity extends AppCompatActivity {
 
                         } catch (IOException e) {
                             // Tratamento de Erro da Localização ---> Erro no Processo
-                            Log.e("GETLOCATION", "Erro na recuperação do endereço " +
+                            Log.e("GETLOCATION", "\nErro na recuperação do endereço " +
                                     "pela Latitude e Longitude.\n" + e);
                         }
                         catch (IllegalArgumentException illegalArgumentException) {
                             // Tratamento de Erro da Localização ---> Formato não aceito
-                            Log.e("TAG???","Latitude = " + location.getLatitude() +
+                            Log.e("ERROR FORMAT","\nLatitude = " + location.getLatitude() +
                                     ", Longitude = " +
                                     location.getLongitude(), illegalArgumentException);
                         }
                         finally {
                             // Imprime o resultado da List de Endereços (ArrayList)
-                            Log.e("LIST ENDEREÇO",addresses.get(0).toString()
+                            Log.i("LIST ENDEREÇO","\n" + addresses.get(0).toString()
                                     + "\n" + addresses.get(0).getAddressLine(0));
-
 
                             // Caso não retorne nenhum endereço
                             if(addresses == null || addresses.isEmpty()){
-                                showAdress.setText("Vazio");
+                                showAddress.setText(R.string.error_searchLocation);
                             }
                             else{
                                 // Exibe o Endereço
-                                showAdress.setText(addresses.get(0).getAddressLine(0));
+                                showAddress.setText(addresses.get(0).getAddressLine(0));
                             }
 
                         }
@@ -113,7 +161,7 @@ public class LocationActivity extends AppCompatActivity {
                 new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.e("TAG", "onFailure: ", e);
+                        Log.e("TAG", "\nonFailure: ", e);
                     }
                 }
             );
