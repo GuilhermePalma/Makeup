7524837e75efeb74e903f7b506a76b249b7cd9ad
Guilherme <guilherme.palma8@etec.sp.gov.br>
2021-08-13 22:20:40 -0300

    Feature: Melhorias no Menu para as Opções de Favoritar

--- Makeup/src/main/java/com/example/maquiagem/view/activities/MainActivity.java
+++ Makeup/src/main/java/com/example/maquiagem/view/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,40 +16,57 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.maquiagem.R;
-import com.example.maquiagem.view.AlertDialogs;
-import com.example.maquiagem.view.fragments.ListMakeup;
+import com.example.maquiagem.controller.CursorMakeup;
+import com.example.maquiagem.controller.DataBaseHelper;
+import com.example.maquiagem.controller.ManagerKeyboard;
+import com.example.maquiagem.model.Makeup;
+import com.example.maquiagem.view.PersonAlertDialogs;
+import com.example.maquiagem.view.fragments.FragmentListMakeup;
 import com.example.maquiagem.view.fragments.SearchMakeup;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int id_profile = R.id.option_profile;
-    private static final int id_config = R.id.option_config;
-    private static final int id_dataUsed = R.id.option_dataUsed;
-    private static final int id_exit = R.id.option_exit;
-    private static final int id_searchMakeup = R.id.option_searchMakeup;
-    private static final int id_allMakeup = R.id.option_allMakeup;
-    private static final int id_favoriteMakeups = R.id.option_favoriteMakeup;
-    private static final int id_popularMakeup = R.id.option_popularMakeup;
-    private static final int id_location = R.id.option_location;
-    private static final int id_sensor = R.id.option_sensor;
+    private final int ID_PROFILE = R.id.option_profile;
+    private final int ID_CONFIG = R.id.option_config;
+    private final int ID_DATA_USED = R.id.option_dataUsed;
+    private final int ID_EXIT = R.id.option_exit;
+    private final int ID_SEARCH_MAKEUP = R.id.option_searchMakeup;
+    private final int ID_HISTORIC_MAKEUP = R.id.option_historicMakeup;
+    private final int ID_FAVORITE_MAKEUPS = R.id.option_favoriteMakeup;
+    private final int ID_MORE_FAVORITES = R.id.option_moreFavorites;
+    private final int ID_LOCATION = R.id.option_location;
+    private final int ID_SENSOR = R.id.option_sensor;
 
     private Toolbar toolbar;
     private DrawerLayout drawer;
     private NavigationView navigationView;
-    private ListMakeup listMakeup;
+    private List<Makeup> listMakeup;
 
-    private AlertDialogs dialogs;
+    private FragmentListMakeup fragmentListMakeup;
+    private CursorMakeup cursorMakeup;
+    private PersonAlertDialogs dialogs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dialogs = new AlertDialogs();
         toolbar = findViewById(R.id.toolBar);
         drawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
+
+        fragmentListMakeup = new FragmentListMakeup();
+        listMakeup = new ArrayList<>();
+        cursorMakeup = new CursorMakeup(getApplicationContext());
+        dialogs = new PersonAlertDialogs(this);
+
+        // Fecha o Teclado ao Iniciar a Activity
+        ManagerKeyboard managerKeyboard = new ManagerKeyboard(this);
+        managerKeyboard.closeKeyboard(this);
 
         // Criação da ToolBar
         toolbar.setTitle(R.string.app_name);
@@ -57,13 +75,16 @@ public class MainActivity extends AppCompatActivity {
         setUpDrawer();
         listenerNavigation();
 
-        if (savedInstanceState == null) {
-            // Define o Background da Main Activity
-            getSupportFragmentManager().beginTransaction().replace(R.id.frame_forFragment,
-                    new SearchMakeup(getApplicationContext())).commit();
-            navigationView.setCheckedItem(R.id.option_searchMakeup);
-        }
+        // Define o Background da Main Activity
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_forFragment,
+                new SearchMakeup(getApplicationContext())).commit();
+
+        // Define o Item que será inicialmente Selecionado
+        navigationView.getMenu().findItem(R.id.option_searchMakeup).setChecked(true);
+        navigationView.getMenu().findItem(R.id.option_searchMakeup).setCheckable(true);
+
     }
+
 
     // Configuração do Menu Lateral
     private void setUpDrawer() {
@@ -75,69 +96,97 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle.syncState();
     }
 
+
     private void listenerNavigation() {
         // Trata o Clique nos Itens
         navigationView.setNavigationItemSelectedListener(item -> {
+
+            // Tira a Seleção dos Itens que não foram Selecionados
+            int sizeMenu = navigationView.getMenu().size();
+            for (int i = 0; i < sizeMenu; i++) {
+                MenuItem item1 = navigationView.getMenu().getItem(i);
+                if (item1.hasSubMenu()) {
+                    // Caso tenha um Sub-menu, acessa eles para retirar a seleção
+                    for (int u = 0; u < item1.getSubMenu().size(); u++) {
+                        item1.getSubMenu().getItem(u).setChecked(false);
+                        item1.getSubMenu().getItem(u).setCheckable(false);
+                    }
+                } else {
+                    navigationView.getMenu().getItem(i).setChecked(false);
+                    navigationView.getMenu().getItem(i).setCheckable(false);
+                }
+            }
+
+            // Seleciona o Item clicado
+            item.setChecked(true);
+            item.setCheckable(true);
+
+            // SwitchCase para verificar qual item foi selecionado
             switch (item.getItemId()) {
-                case id_profile:
+                case ID_PROFILE:
                     startActivity(new Intent(MainActivity.this, ProfileActivity.class));
                     break;
 
-                case id_favoriteMakeups:
-                    listMakeup = ListMakeup.newInstance("Maquiagens Favoritas");
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_forFragment,
-                            listMakeup).commit();
+                case ID_FAVORITE_MAKEUPS:
+                    String select_favorite = String.format("SELECT * FROM %1$s WHERE %2$s='%3$s'",
+                            DataBaseHelper.TABLE_MAKEUP, DataBaseHelper.IS_FAVORITE_MAKEUP, "true");
+
+                    listMakeup = cursorMakeup.selectDataBase(select_favorite);
+                    setUpListFragment(listMakeup, FragmentListMakeup.TYPE_FAVORITE);
                     break;
 
-                case id_searchMakeup:
+                case ID_SEARCH_MAKEUP:
                     getSupportFragmentManager().beginTransaction().replace(R.id.frame_forFragment,
                             new SearchMakeup(getApplicationContext())).commit();
                     break;
 
-                case id_popularMakeup:
-                    listMakeup = ListMakeup.newInstance("Maquiagens mais Buscadas");
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_forFragment,
-                            listMakeup).commit();
+                case ID_MORE_FAVORITES:
+                    // Todo: Implementar metodo da API das Maquiagens mais Favoritadas
+                    String select_popular = String.format("SELECT * FROM %s",
+                            DataBaseHelper.TABLE_MAKEUP);
+                    listMakeup = cursorMakeup.selectDataBase(select_popular);
+                    setUpListFragment(listMakeup, FragmentListMakeup.TYPE_MORE_LIKED);
                     break;
 
-                case id_allMakeup:
-                    listMakeup = ListMakeup.newInstance("Todas as Maquiagens");
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_forFragment,
-                            listMakeup).commit();
+                case ID_HISTORIC_MAKEUP:
+                    String select_historic = String.format("SELECT * FROM %s",
+                            DataBaseHelper.TABLE_MAKEUP);
+                    listMakeup = cursorMakeup.selectDataBase(select_historic);
+                    setUpListFragment(listMakeup, FragmentListMakeup.TYPE_HISTORIC);
                     break;
 
-                case id_location:
+                case ID_LOCATION:
                     // Possui Conexão
                     if (connectionAvailable()) {
                         // GPS ativo
                         if (gpsAvailable()) {
                             startActivity(new Intent(this, LocationActivity.class));
                         } else {
-                            dialogs.message(this, getString(R.string.title_noGps),
-                                    getString(R.string.error_noGps)).show();
+                            dialogs.message(getString(R.string.title_noGps), getString(R.string.error_noGps))
+                                    .show();
                         }
                     } else {
-                        dialogs.message(this, getString(R.string.title_noConnection),
-                                getString(R.string.error_connection)).show();
+                        dialogs.message(getString(R.string.title_noConnection), getString(R.string.error_connection))
+                                .show();
                     }
                     break;
 
-                case id_sensor:
+                case ID_SENSOR:
                     startActivity(new Intent(MainActivity.this,
                             SensorActivity.class));
                     break;
 
-                case id_config:
+                case ID_CONFIG:
                     startActivity(new Intent(MainActivity.this,
                             ConfigurationActivity.class));
                     break;
 
-                case id_dataUsed:
+                case ID_DATA_USED:
                     startActivity(new Intent(MainActivity.this,
                             DataApplicationActivity.class));
                     break;
 
-                case id_exit:
+                case ID_EXIT:
                     // todo: deslogar ---> Implementar depois do Cadastro/Login
                     break;
 
@@ -149,6 +198,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setUpListFragment(List<Makeup> makeups, String type_fragment) {
+        if (makeups == null) {
+            dialogs.message(getString(R.string.title_noData),
+                    getString(R.string.error_tableEmpty)).show();
+        } else {
+
+            fragmentListMakeup = FragmentListMakeup.newInstance(this, listMakeup,
+                    type_fragment);
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_forFragment,
+                    fragmentListMakeup).commit();
+        }
+    }
 
     // Caso clique no botão voltar
     @Override
